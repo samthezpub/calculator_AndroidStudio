a63d2f2c56493c95eb4ce3ac79da406e3924621f
Vladimir <70057256+samthezpub@users.noreply.github.com>
2023-10-30 23:08:17 +0300

    Clear method added, switch operator, nums

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void clearNum(View view) {
+        TextView num = findViewById(R.id.textView);
+        num.setText("0");
+    }
+
+    public void switchOperator(View view) {
+        TextView num = findViewById(R.id.textView);
+
+        Integer integerText = Integer.parseInt(num.getText().toString());
+        String switchedText = String.valueOf((-integerText));
+        num.setText(switchedText);
+    }
+
+    public void addNumToTextValue(View view){
+        TextView num = findViewById(R.id.textView);
+        Button button = (Button) view; // Преобразование 'view' к типу Button
+
+        char numText = num.getText().charAt(0);
+
+        if (num.getText().charAt(0) == '0'){
+            num.setText(button.getText());
+        }
+        else{
+            num.setText(num.getText().toString() + button.getText());
+        }
+
+
+
     }
 }
